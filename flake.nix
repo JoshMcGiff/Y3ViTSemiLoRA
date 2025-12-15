4deccaf18954ad5803c8da16c7d0b39cda3fc396
robshan5 <robshanahan5@proton.me>
2025-12-15 16:52:55 +0000

    added flake file

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+    description = "Python development environment";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = import nixpkgs {
+                    inherit system;
+                };
+                pythonEnv = pkgs.python3.withPackages(p: with p; [
+                    numpy
+                    pandas
+                    scikit-learn
+                    jupyterlab
+                    matplotlib
+                    torch
+                    torchvision
+                    transformers
+                    pillow
+                    tqdm
+                ]);
+            in
+                {
+                devShell = pkgs.mkShell {
+                    packages = [
+                        pythonEnv
+                    ];
+                    shellHook = ''
+            echo "Python environment activated"
+            python --version
+            exec zsh
+            '';
+                };
+            });
+}
